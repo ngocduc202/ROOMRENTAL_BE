2be03ddc829197c9a4d3741cfd7de4d0afc4ab76
ngocduc202 <ngocduc1542002@gmail.com>
2024-11-25 22:03:08 +0700

    fix getPostLimit by Operator

--- src/models/post.js
+++ src/models/post.js
@@ -40,10 +40,13 @@ module.exports = (sequelize, DataTypes) => {
     categoryCode: DataTypes.STRING,
     priceCode: DataTypes.STRING,
     areaCode: DataTypes.STRING,
+    provinceCode: DataTypes.STRING,
     description : DataTypes.TEXT,
     userId : DataTypes.STRING,
     overviewId :  DataTypes.STRING,
-    imagesId : DataTypes.STRING
+    imagesId : DataTypes.STRING,
+    priceNumber: DataTypes.FLOAT,
+    areaNumber: DataTypes.FLOAT,
   }, {
     sequelize,
     modelName: 'Post',
