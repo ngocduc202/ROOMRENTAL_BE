c14a4ec5c85528332765bd65022a66d5bb0ee31b
ngocduc202 <ngocduc1542002@gmail.com>
2024-11-30 22:56:04 +0700

    handle createNewPost service

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,9 +1,13 @@
 import express from "express";
 import * as controller from "../controllers/post"
+import verifyToken from "../middlewares/verifyToken";
 
 const router = express.Router()
 router.get('/all' , controller.getPosts)
 router.get('/limit' , controller.getPostsLimit)
 router.get('/new-post' , controller.getNewPosts)
+
+router.use(verifyToken)
+router.post('/create-new' , controller.createNewPost)
 
 export default router
